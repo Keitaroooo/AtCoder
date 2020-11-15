e4c378ca5368c442ea2d0cc121351068e72311ff
keitaro <keitaro.cryptocurrency@gmail.com>
2020-11-15 22:44:10 +0900

    ABC183

---
+++ AtCoderBeginnerContest/183/A.cpp
@@ -0,0 +1,21 @@
+#define _USE_MATH_DEFINES
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <set>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int x;
+    cin >> x;
+    if(x >= 0){
+        cout << x << endl;
+    }else{
+        cout << 0 << endl;
+    }
+    return 0;
+}
